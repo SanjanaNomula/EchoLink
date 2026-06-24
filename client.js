fe453f74dc9d076cb0c8211dc85c516589a7ddad
Sanjana Nomula <140239776+SanjanaNomula@users.noreply.github.com>
2026-06-24 15:57:38 +0530

    Add custom peer ID support

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 let peer;
 
 const connectBtn = document.getElementById("connectBtn");
+const peerIdInput = document.getElementById("peerId");
 
 connectBtn.addEventListener("click", () => {
-    peer = new Peer();
+    peer = new Peer(peerIdInput.value);
 
     console.log("Peer initialized");
 });
