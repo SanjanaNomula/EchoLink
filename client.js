500dd28746da28d67883531f40ee620ab0076106
Sanjana Nomula <140239776+SanjanaNomula@users.noreply.github.com>
2026-06-24 16:09:33 +0530

    Display connection status

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ let peer;
 
 const connectBtn = document.getElementById("connectBtn");
 const peerIdInput = document.getElementById("peerId");
+const statusText = document.getElementById("status");
 
 async function setupMicrophone() {
     try {
@@ -19,7 +20,13 @@ async function setupMicrophone() {
 connectBtn.addEventListener("click", async () => {
     const stream = await setupMicrophone();
 
+    statusText.textContent = "Connecting...";
+    
     peer = new Peer(peerIdInput.value);
+
+    peer.on("open", () => {
+    statusText.textContent = "Connected";
+});
 
     console.log("Peer initialized");
 });
